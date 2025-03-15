c36c2da7b8e8bef108d23eaba9778cd975a0c27b
stelasusanina <114248714+stelasusanina@users.noreply.github.com>
2025-03-15 16:14:29 +0200

    Add project files.

---
+++ Welcome/Model/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Welcome.Others;
+
+namespace Welcome.Model
+{
+	public class User
+	{
+        public string Name { get; set; }
+		public string Password { get; set; }
+		public UserRolesEnum Role { get; set; }
+		public User(string name, string password, UserRolesEnum role)
+		{
+			Name = name;
+			Password = password;
+			Role = role;
+		}
+    }
+}
---
+++ Welcome/Program.cs
@@ -0,0 +1,19 @@
+﻿using Welcome.Model;
+using Welcome.View;
+using Welcome.ViewModel;
+using Welcome.Others;
+
+namespace Welcome
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			User user = new User("Stela", "stela123", UserRolesEnum.STUDENT);
+			UserViewModel userViewModel = new UserViewModel(user);
+			UserView userView = new UserView(userViewModel);
+
+			userView.Display();
+		}
+	}
+}
